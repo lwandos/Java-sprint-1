d639441c6006f87e58fd85ec8cc5d3bd8c29d823
lwandos <62343187+lwandos@users.noreply.github.com>
2020-07-17 13:56:57 +0200

    Create mzamomtsha.java

---
+++ mzamomtsha.java
@@ -0,0 +1,29 @@
+package sprint
+
+import java.util.scanner;
+
+public class MyClass{
+   
+     public static void main(String[] args) {
+         Scanner scan = new Scanner (System.in);
+         System.out.println("Enter name: ");
+         string user_input_name = scan.nextStr();
+         
+          System.out.println("Enter surname: ");
+         string user_input_surname = scan.nextStr();
+         
+          System.out.println("Enter learner number: ");
+         int user_input_number = scan.nextInt();
+         
+          System.out.println("Enter Date of Birth: ");
+         string user_input_DOB = scan.nextStr();
+         
+          System.out.println("Parents contact details: ");
+         int user_input_name = scan.nextInt();
+         
+          System.out.println("Enter your adress: ");
+         string user_input_name = scan.nextStr();
+         
+          
+     }
+     }
